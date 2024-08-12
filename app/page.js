f83c6181250879d75d852c11b64742b16fdef6c4
yindrag0n <jorden.demerr.edu@gmail.com>
2024-08-12 14:15:43 -0400

    deploy

--- app/page.js
+++ app/page.js
@@ -1,51 +1,68 @@
-"use client";
+'use client';
+import React, { useState, useEffect } from 'react';
+import {
+  collection,
+  addDoc,
+  getDoc,
+  querySnapshot,
+  query,
+  onSnapshot,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
+import { db } from './firebase';
 
-import React, { useState, useEffect } from "react";
-import { collection, addDoc, querySnapshot, onSnapshot } from "firebase/firestore";
-import { db } from "./firebase";
 
 export default function Home() {
   const [items, setItems] = useState([
+    // { name: 'Coffee', price: 4.95 },
+    // { name: 'Movie', price: 24.95 },
+    // { name: 'candy', price: 7.95 },
   ]);
-
   const [newItem, setNewItem] = useState({ name: '', price: '' });
   const [total, setTotal] = useState(0);
 
   // Add item to database
-  const addItem =  async (e) => {
+  const addItem = async (e) => {
     e.preventDefault();
     if (newItem.name !== '' && newItem.price !== '') {
-      await addDoc(collection(db, "items"), {
+      // setItems([...items, newItem]);
+      await addDoc(collection(db, 'items'), {
         name: newItem.name.trim(),
         price: newItem.price,
       });
-      setNewItem({name: '', price: ''});
+      setNewItem({ name: '', price: '' });
     }
   };
 
-  // Read items from database
-  useEffect(() => {
-    const q = querySnapshot(collection(db, "items"));
+   // Read items from database
+   useEffect(() => {
+    const q = query(collection(db, 'items'));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let itemsArr = [];
 
       querySnapshot.forEach((doc) => {
-        itemsArr.push({...doc.data(), id: doc.id});
+        itemsArr.push({ ...doc.data(), id: doc.id });
       });
       setItems(itemsArr);
 
-      // Read total from itemsArray
+      // Read total from itemsArr
       const calculateTotal = () => {
-        const totalPrice = itemsArr.reduce((sum, item) => sum + parseFloat(item.price), 0);
+        const totalPrice = itemsArr.reduce(
+          (sum, item) => sum + parseFloat(item.price),
+          0
+        );
         setTotal(totalPrice);
       };
       calculateTotal();
       return () => unsubscribe();
-
     });
   }, []);
 
   // Delete items from database
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, 'items', id));
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -76,12 +93,18 @@ export default function Home() {
           </form>
           <ul>
             {items.map((item, id) => (
-              <li key={id} className="my-4 w-full flex justify-between">
+              <li 
+                key={id} 
+                className="my-4 w-full flex justify-between"
+              >
                 <div className="p-4 w-full flex justify-between">
                   <span className="capitalize">{item.name}</span>
                   <span>${item.price}</span>
                 </div>
-                <button className="ml-8 p-4 border-l-2 border-slate-400 hover:bg-slate-400 w-16 hover:text-slate-50">
+                <button 
+                  onClick={() => deleteItem(item.id)}
+                  className="ml-8 p-4 border-l-2 border-slate-400 hover:bg-slate-400 w-16 hover:text-slate-50"
+                >
                   X
                 </button>
               </li>
@@ -90,11 +113,11 @@ export default function Home() {
           {items.length < 1 ? ("") : (
             <div className="flex justify-between p-3">
               <span>Total</span>
-              <span>$(total)</span>
+              <span>${total}</span>
             </div>
           )}
         </div>
       </div>
     </main>
   );
-}
+};
